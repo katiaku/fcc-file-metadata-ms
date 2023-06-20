0a0f83862e5db6f0b61caf2976e047aed52bc7dc
Katia <kushnir.ekaterina@gmail.com>
2023-06-20 13:31:27 +0200

    Add endpoints

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var cors = require('cors');
+var multer  = require('multer');
+var fs = require('fs');
 require('dotenv').config()
 
 var app = express();
+
+var upload = multer({ dest: 'uploads/' });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,8 +15,24 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/get-file-info', upload.single('datafile'), function (req, res, next) {
+  var fl = req.file;
+  
+  return res.send({ "name": fl.originalname, "size": fl.size});
+});
 
-
+app.get('/', function (req, res) {
+  fs.readFile('input-form.html', function (err, data) {
+      if(err !== null) console.log("ERROR Uploading");
+      
+      res.writeHead(200, {
+          'Content-Type': 'text/html',
+              'Content-Length': data.length
+      });
+      res.write(data);
+      res.end();
+  });    
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
