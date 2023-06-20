f6e7e8416c9589dce7f386575b9045e1cd502a25
Katia <kushnir.ekaterina@gmail.com>
2023-06-20 14:10:08 +0200

    Add api

--- index.js
+++ index.js
@@ -1,40 +1,28 @@
+require('dotenv').config();
+
 var express = require('express');
 var cors = require('cors');
-var multer  = require('multer');
-var fs = require('fs');
-require('dotenv').config()
-
+var multer = require('multer');
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage });
 var app = express();
-
-var upload = multer({ dest: 'uploads/' });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
-
-app.post('/get-file-info', upload.single('datafile'), function (req, res, next) {
-  var fl = req.file;
-  
-  return res.send({ "name": fl.originalname, "size": fl.size});
-});
-
-app.get('/', function (req, res) {
-  fs.readFile('input-form.html', function (err, data) {
-      if(err !== null) console.log("ERROR Uploading");
-      
-      res.writeHead(200, {
-          'Content-Type': 'text/html',
-              'Content-Length': data.length
-      });
-      res.write(data);
-      res.end();
-  });    
+    
+app.post('/api/fileanalyse',upload.single('upfile'), function(req, res){
+  res.json({
+    'name' : req.file.originalname,
+    'type' : req.file.mimetype,
+    'size' : req.file.size
+  });
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, function() {
   console.log('Your app is listening on port ' + port)
 });
